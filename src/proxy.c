d8118420ef7e51cd3fa26cc080b2f1ce151b2e1e
James O. D. Hunt <james.o.hunt@intel.com>
2016-10-11 10:48:35 +0100

    Add message if proxy wait fails.
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- src/proxy.c
+++ src/proxy.c
@@ -637,6 +637,7 @@ cc_proxy_hyper_pod_create (struct cc_oci_config *config)
 	}
 
 	if (! cc_proxy_wait_until_ready (config)) {
+		g_critical ("failed to wait for proxy %s", CC_OCI_PROXY);
 		goto out;
 	}
 
